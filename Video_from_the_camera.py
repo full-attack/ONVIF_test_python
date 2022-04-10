0c29294479c7a932b30070437324bf9b4d310afc
easy_il <75256407+full-attack@users.noreply.github.com>
2022-04-10 17:16:02 +0300

    Update Video_from_the_camera.py

--- Video_from_the_camera.py
+++ Video_from_the_camera.py
@@ -1,32 +1,30 @@
-import numpy as np
 import cv2
 
+## opening videocapture
 cap = cv2.VideoCapture('rtsp://admin:Supervisor@172.18.191.177:554/live/0/MAIN/')
 
-size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+## some videowriter props
+sz = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
 int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 
-
 fps = 20
-#fourcc = cv2.cv.CV_FOURCC('m', 'p', '4', 'v') # note the lower case
+#fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+#fourcc = cv2.VideoWriter_fourcc('m', 'p', 'e', 'g')
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
+## open and set props
 vout = cv2.VideoWriter()
-success = vout.open('output.mp4',fourcc,fps,size,True)
+vout.open('output.mp4',fourcc,fps,sz,True)
+
+#cnt = 0
 while True:
-ret, frame = cap.read()
-if not ret:
-break
-
-frame = cv2.flip(frame, -1)
-
+#cnt += 1
+#print(cnt)
+_, frame = cap.read()
+cv2.putText(frame, "Hello World!!!", (10, 20), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,0), 1, cv2.LINE_AA)
 vout.write(frame)
-
-cv2.imshow('frame', frame)
-
 if cv2.waitKey(1) & 0xFF == ord('q'):
 break
 
-cap.release()
 vout.release()
-cv2.destroyAllWindows()
+cap.release()
